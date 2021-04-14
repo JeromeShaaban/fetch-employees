d189d9933a2782dd819bbbdb08b79e9efd9736c7
JaddN7 <jerome.shaaban@gmail.com>
2021-04-14 22:57:17 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useEffect, useState } from "react"
+import QuoteCard from "./components/QuoteCard"
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [quote, setQuote] = useState({
+        quote:
+            "Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?",
+        character: "Abe Simpson",
+        image:
+            "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593",
+        characterDirection: "Right",
+    })
+
+    const handleClick = () => {
+        axios
+            .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+            .then((response) => response.data)
+            .then((data) => {
+                setQuote(data[0])
+            })
+    }
+
+    return (
+        <div className="App">
+            <QuoteCard key={quote._id} {...quote} />
+            <button type="button" onClick={handleClick}>
+                Get a new quote !
+            </button>
+        </div>
+    )
 }
 
-export default App;
+export default App
